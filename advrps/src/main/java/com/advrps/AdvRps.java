b2cfdef6ecb37699f16255d49eb89c156d7c392b
mdhand <mdhand@gmail.com>
2019-12-08 14:42:18 -0500

    initial project creation

---
+++ advrps/src/main/java/com/advrps/AdvRps.java
@@ -0,0 +1,11 @@
+package com.advrps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvRps {
+    public static void main(String[] args) {
+        SpringApplication.run(AdvRps.class, args);
+    }
+}
