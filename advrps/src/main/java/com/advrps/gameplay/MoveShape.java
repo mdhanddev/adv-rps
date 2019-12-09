605f6387954fb37ec6fd3b741e3fa4e0ef3dd7ad
mdhand <mdhand@gmail.com>
2019-12-08 23:19:18 -0500

    adjusted RPS "wins-against" lists

--- advrps/src/main/java/com/advrps/gameplay/MoveShape.java
+++ advrps/src/main/java/com/advrps/gameplay/MoveShape.java
@@ -8,11 +8,18 @@ import org.apache.commons.lang.NotImplementedException;
 public enum MoveShape {
     //this got tricky because I can't reference the MoveShapes that each MoveShape beats
     //    until they are defined, so it ended up being a bit hacky
-    ROCK(1, List.of(3, 5)),
-    PAPER(2, List.of(2, 4)),
-    SCISSORS(3, List.of(1, 5)),
+
+    // https://the-big-bang-theory.com/rock-paper-scissors-lizard-spock/
+    // scissors cuts paper, scissors decapitates lizard
+    // paper covers rock, paper disproves Spock
+    // rock crushes lizard, rock crushes scissors
+    // lizard poisons Spock, lizard eats paper
+    // Spock smashes scissors, Spock vaporizes rock
+    ROCK(1, List.of(5, 3)),
+    PAPER(2, List.of(1, 4)),
+    SCISSORS(3, List.of(2, 5)),
     SPOCK(4, List.of(3, 1)),
-    LIZARD(5, List.of(2, 4));
+    LIZARD(5, List.of(4, 2));
 
     // Pokemon types, or etc
 
